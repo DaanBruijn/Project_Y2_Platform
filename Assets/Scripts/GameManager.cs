2ff13cbff38ff9fd184d3fdc891a7a0128866492
DaanBruijn <332951@student.mboutrecht.nl>
2022-09-20 14:09:01 +0200

    End Screen added to version1 and merged with main branch

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +10,21 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI Scoretext;
     private GameObject textObject;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        //ScoreSystem
         textObject = GameObject.Find("Scoretext");
         Scoretext = textObject.GetComponent<TextMeshProUGUI>();
         Scoretext.text = "Score: " + score;
+        
+        //EndScreen
+        Debug.Log("im GameManager");
+        PlayerPrefs.SetInt("score",5);
+        PlayerPrefs.SetFloat("time",10.50f);
+        PlayerPrefs.SetFloat("iets anders", PlayerPrefs.GetFloat("iets anders") + 1);
     }
 
     // Update is called once per frame
@@ -25,5 +35,22 @@ public class GameManager : MonoBehaviour
             score += 15;
             Scoretext.text = "Score: " + score;
         }
+    }
+    
+    public void LoadNextScene()
+    {
+        //This will load the next scene in the build settings (edit with ctrl + shift + b)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Finn");
+    }
+    
+    public void Win()
+    {
+        Debug.Log("Win");
+        SceneManager.LoadScene("Win");
     }
 }
