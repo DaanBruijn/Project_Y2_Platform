c2474fa610737e63e22829bf6d3863d8c7f33a4b
SimonPras <332959@student.mboutrecht.nl>
2022-09-27 14:06:11 +0200

    Improved score system
    The score is now on the top-left side of the screen instead of the top and the score now has an icon

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Scoretext.text = "Score: " + score;
+        Scoretext.text = "     " + score;
         if (Input.GetKeyDown(KeyCode.K))
         {
             score += 15;
