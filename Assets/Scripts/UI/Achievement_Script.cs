9f616fcbd22f1d88592cf4c179d021d6690feb78
DaanBruijn <332951@student.mboutrecht.nl>
2022-11-30 10:26:29 +0100

    Achievement Commit. Achievement scene made that unlocks when you got the Achievement in the level

---
+++ Assets/Scripts/UI/Achievement_Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Achievement;
+using TMPro;
+
+public class Achievement_Script : MonoBehaviour
+{
+    [Header("Achievement")]
+    [SerializeField] private AchievementObject achievement;
+    [SerializeField] private TMP_Text achievementBoxText;
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Animation with the Correct Text
+            achievementBoxText.text = achievement.achievementName;
+            AchievementManager.Instance.StartAnimation();
+            
+            //Sets Playerpref from ScriptableObject
+            PlayerPrefs.SetInt(achievement.playerPrefName, 1);
+        }
+    }
+}
